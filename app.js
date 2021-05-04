92b390316fe0050647a24d14c1fd0eeb13525b05
tsotnemaisuradze <tsotne.maisa@gmail.com>
2021-05-04 18:47:22 +0400

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json())
+
+app.get('/', (req, res) => {
+    res.json("hello")
+})
+
+app.post('/register', (request, response) => {
+    // response.send('Hello akaki!')
+    let result = request.body;
+    result.id = 1234567;
+    response.json(result);
+})
+
+app.get('/logout', (req, res) => {
+    res.send('Hello tsotne!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
