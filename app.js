8c3f7a031a06fd971ccfbd6250ebdd4a2f95bcf4
tsotne932 <tsotne.maisa@gmail.com>
2022-04-27 10:49:47 +0400

    port

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const port = 3000;
+const port = 7777;
 
 app.use(bodyParser.json())
 app.use(express.static('public'))
