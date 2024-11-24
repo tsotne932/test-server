59392691bf12999e0868a874f5f2000ff5bd11dd
tsotne932 <tsotne.maisa@gmail.com>
2024-11-25 00:07:23 +0400

    aba

--- app.js
+++ app.js
@@ -1,20 +1,19 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const app = express();
+var express = require('express');
+var deeplink = require('node-deeplink');
+ 
+var app = express();
+ 
+app.get(
+  '/deeplink',
+  deeplink({
+    fallback: 'https://orinabiji.ge',
+    android_package_name: 'ge.orinabiji.shop',
+    ios_store_link:
+      'https://itunes.apple.com/us/app/cups-unlimited-coffee/id556462755?mt=8&uo=4'
+  })
+);
 const port = 3003;
 
-app.use(bodyParser.json())
-app.use(express.static('public'))
-app.post('/register', (request, response) => {
-    // response.send('Hello akaki!')
-    let result = request.body;
-    result.id = 1234567;
-    response.json(result);
-})
-
-app.get('/logout', (req, res) => {
-    res.send('Hello tsotne!')
-})
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
